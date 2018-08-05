1d80069a4b30b1f4de443cd67e02ccc8bbdc21d2
bitowl <git@bitowl.de>
2018-08-05 22:05:47 +0200

    Update to NodeCG 1.1 and dark theme

---
+++ extension/lib/milliseconds.js
@@ -0,0 +1,14 @@
+
+// https://github.com/HenrikJoreteg/milliseconds/blob/master/milliseconds.js
+function calc(m) {
+    return function(n) { return Math.round(n * m); };
+};
+module.exports = {
+    seconds: calc(1e3),
+    minutes: calc(6e4),
+    hours: calc(36e5),
+    days: calc(864e5),
+    weeks: calc(6048e5),
+    months: calc(26298e5),
+    years: calc(315576e5)
+};
